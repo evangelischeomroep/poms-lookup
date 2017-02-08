aaa3c0eb11f9e71a1d3d4e8d7597cf2ac96ee106
Leon de Rijke <leon@leonderijke.nl>
2017-02-08 15:25:23 +0100

    Basic app layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
-        <div className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h2>Welcome to React</h2>
+        <header className='App-header'>
+          <h1>POMS Lookup</h1>
+        </header>
+        <div className='App-content'>
+          <p className='App-intro'>
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
         </div>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <footer className='App-footer'>
+          <p>POMS Lookup</p>
+        </footer>
       </div>
     )
   }
