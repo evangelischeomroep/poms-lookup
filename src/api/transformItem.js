1e0d08f1e5b3ce761bd8b58ee15d7da809a5deec
Leon de Rijke <leon@leonderijke.nl>
2017-02-14 11:21:07 +0100

    Format date

--- src/api/transformItem.js
+++ src/api/transformItem.js
@@ -2,7 +2,7 @@ const transformItem = (item) => {
   return {
     ...item,
     title: item.titles[0].value,
-    date: item.sortDate
+    date: new Date(item.sortDate).toLocaleDateString()
   }
 }
 
