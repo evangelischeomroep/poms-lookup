a6fb8c215a4222c8c3af9bfa376df4fc7e5dd4e4
Leon de Rijke <leon@leonderijke.nl>
2017-02-15 11:50:38 +0100

    Add SearchForm tests

--- src/containers/SearchForm.test.js
+++ src/containers/SearchForm.test.js
@@ -1,10 +1,19 @@
 /* eslint-env jest */
 
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
 import SearchForm from './SearchForm'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<SearchForm />, div)
+  shallow(<SearchForm />)
+})
+
+it('calls onSubmit with the entered text', () => {
+  const onSubmit = jest.fn()
+  const wrapper = shallow(<SearchForm onSubmit={onSubmit} />)
+  const query = { text: 'test' }
+
+  wrapper.setState(query)
+  wrapper.simulate('submit', { preventDefault: () => {} })
+  expect(onSubmit).toHaveBeenCalledWith(query)
 })
