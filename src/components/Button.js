7d57fa9c432db6e658346c4a836c810c06181cc8
Leon de Rijke <leon@leonderijke.nl>
2017-02-15 10:50:29 +0100

    Make Button component simpler

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,9 @@
 import React from 'react'
-import classnames from 'classnames'
 
 import './Button.css'
 
-const Button = ({ children, className, ...other }) => {
-  const classes = classnames({
-    Button: true
-  }, className)
-
-  return (
-    <button className={classes} {...other}>{children}</button>
-  )
-}
+const Button = ({ children, ...other }) => (
+  <button className='Button' {...other}>{children}</button>
+)
 
 export default Button
